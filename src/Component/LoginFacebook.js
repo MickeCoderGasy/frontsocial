148e2f6b79628a895cc8360f18ffa2ea6d39f703
MickeCoderGasy <micka.ram22@gmail.com>
2023-07-16 12:06:28 +0200

    Initial commit

---
+++ src/Component/LoginFacebook.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+const LoginFacebook = () => {
+  const handleFacebookLogin = (response) => {
+    if (response.status !== 'unknown') {
+      // User is logged in with Facebook
+      const accessToken = response.accessToken;
+      // Send the access token to the Django backend for authentication
+      fetch('http://localhost:5000/social/login/facebook/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ access_token: accessToken }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Handle the response from the Django backend
+          console.log(data);
+        })
+        .catch((error) => {
+          // Handle any errors
+          console.error(error);
+        });
+    } else {
+      // User is not logged in with Facebook or cancelled the login process
+      console.log('User is not logged in with Facebook');
+    }
+  };
+
+  return (
+    <div>
+      <FacebookLogin
+        appId="216353421374535"
+        fields="name,email,picture"
+        callback={handleFacebookLogin}
+      />
+    </div>
+  );
+};
+
+export default LoginFacebook;
