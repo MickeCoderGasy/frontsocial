f333f0e43d0f1ad250f203521f12eac79cf45e28
MickeCoderGasy <micka.ram22@gmail.com>
2023-07-17 00:42:18 +0200

    Update LoginFacebook.js

--- src/Component/LoginFacebook.js
+++ src/Component/LoginFacebook.js
@@ -7,7 +7,7 @@ const LoginFacebook = () => {
       // User is logged in with Facebook
       const accessToken = response.accessToken;
       // Send the access token to the Django backend for authentication
-      fetch('https://557f-41-188-36-110.ngrok-free.app/social/api/login/facebook/', {
+      fetch('https://ff3b-41-188-36-110.ngrok-free.app/social/api/login/facebook/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
